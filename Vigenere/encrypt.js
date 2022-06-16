1f17408edb9829fe9db95d684f8e1443733a16bf
garfieldw33 <kesiyun@outlook.com>
2022-06-16 15:13:47 +0800

    New interactive page

--- Vigenere/encrypt.js
+++ Vigenere/encrypt.js
@@ -3,37 +3,51 @@ var key="HeLLOworld";
 var original="GARFIELD AND WENDY ARE STUDENTS of DEREK";
 //var key="BIG";
 //var original="THE BUTCHER THE BAKER AND THE CANDLESTICK MAKER";
+function input() {
+    original = document.getElementById("texts").value;  
+    key = document.getElementById("keys").value;  
 
-key=key.toUpperCase();
-var cleaned = original.split(" ").join("").toUpperCase();
-var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";//0-25
+    encrypted=mainF(key,original);
+    document.getElementById("encrypted_results").innerHTML = encrypted;
+    window.alert("Result:"+encrypted);
 
-function nextLetter(letter,num)
-{
-    let place=alphabet.indexOf(letter)+num;
-    if(place<=25) {
-        return alphabet[place];
-    }else{
-        return alphabet[place-26];
+}
+
+function mainF(key,original){
+    key=key.toUpperCase();
+    var cleaned = original.split(" ").join("").toUpperCase();
+    var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";//0-25
+    
+    function nextLetter(letter,num)
+    {
+        let place=alphabet.indexOf(letter)+num;
+        if(place<=25) {
+            return alphabet[place];
+        }else{
+            return alphabet[place-26];
+        }
     }
+    
+    var cleaned_len=cleaned.length;
+    var key_len=key.length;
+    
+    var key_new="",result="";
+    
+    for (let i=0;i<cleaned_len;i++){
+        key_new=key_new+key[i%key_len]
+    }
+    
+    for (let i=0;i<cleaned_len;i++){
+        key_letter=key_new[i]
+        ori_num=alphabet.indexOf(cleaned[i])
+        result=result+nextLetter(key_letter,ori_num)
+    }
+    
+   return result;
 }
 
-var cleaned_len=cleaned.length;
-var key_len=key.length;
 
-var key_new="",result="";
+//encrypted=mainF(key,original);
 
-for (let i=0;i<cleaned_len;i++){
-    key_new=key_new+key[i%key_len]
-}
-
-for (let i=0;i<cleaned_len;i++){
-    key_letter=key_new[i]
-    ori_num=alphabet.indexOf(cleaned[i])
-    result=result+nextLetter(key_letter,ori_num)
-}
-
-
-window.alert(cleaned+"\n"+key);
-window.alert(result);
-
+//window.alert(original+"\n"+key);
+//window.alert(encrypted);
