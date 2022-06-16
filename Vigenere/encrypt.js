c0b690ad2be748d0d9c69a1801c57de5df21e072
garfieldw33 <kesiyun@outlook.com>
2022-06-16 12:02:57 +0800

    Vigenere encrypt

---
+++ Vigenere/encrypt.js
@@ -0,0 +1,39 @@
+
+var key="HeLLOworld";
+var original="GARFIELD AND WENDY ARE STUDENTS of DEREK";
+//var key="BIG";
+//var original="THE BUTCHER THE BAKER AND THE CANDLESTICK MAKER";
+
+key=key.toUpperCase();
+var cleaned = original.split(" ").join("").toUpperCase();
+var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";//0-25
+
+function nextLetter(letter,num)
+{
+    let place=alphabet.indexOf(letter)+num;
+    if(place<=25) {
+        return alphabet[place];
+    }else{
+        return alphabet[place-26];
+    }
+}
+
+var cleaned_len=cleaned.length;
+var key_len=key.length;
+
+var key_new="",result="";
+
+for (let i=0;i<cleaned_len;i++){
+    key_new=key_new+key[i%key_len]
+}
+
+for (let i=0;i<cleaned_len;i++){
+    key_letter=key_new[i]
+    ori_num=alphabet.indexOf(cleaned[i])
+    result=result+nextLetter(key_letter,ori_num)
+}
+
+
+window.alert(cleaned+"\n"+key);
+window.alert(result);
+
